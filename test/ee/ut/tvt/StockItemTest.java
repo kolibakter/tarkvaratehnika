b361098989485493ef063d8d115392cc85b5aaf7
tennex <sander@smail.ee>
2014-12-02 15:39:29 +0200

    StockItemTest fix

--- test/ee/ut/tvt/StockItemTest.java
+++ test/ee/ut/tvt/StockItemTest.java
@@ -17,20 +17,32 @@ public class StockItemTest {
 	@Test
 	public void testClone() {
 		StockItem testItem2 = (StockItem) testItem.clone();
-		if(testItem.getId().equals(testItem2.getId()))
-			if(testItem.getName().equals(testItem2.getName()))
-				if(testItem.getDescription().equals(testItem2.getDescription()))
-					if(testItem.getPrice() == testItem2.getPrice())
-						if(testItem.getQuantity() == testItem2.getQuantity())
-							Assert.assertTrue(true);
+		if(testItem.getId().equals(testItem2.getId())) {
+			Assert.assertTrue(true);}	
+		if(testItem.getName().equals(testItem2.getName())) {
+			Assert.assertTrue(true);}
+		if(testItem.getDescription().equals(testItem2.getDescription())) {
+			Assert.assertTrue(true);}
+		if(testItem.getPrice() == testItem2.getPrice()) {
+			Assert.assertTrue(true);}
+		if(testItem.getQuantity() == testItem2.getQuantity()){
+			Assert.assertTrue(true);}
+		}
+							
 	}
 	
 	@Test
 	public void testGetColumn() {
-		if((long) testItem.getColumn(0) == 100)
-			if(((String) testItem.getColumn(1)).equals("testItem"))
-				if((double) testItem.getColumn(2) == 100.0)
-					if((int) testItem.getColumn(3) == 10)
-						Assert.assertTrue(true);
+		if((long) testItem.getColumn(0) == 100){
+			Assert.assertTrue(true);}
+		
+		if(((String) testItem.getColumn(1)).equals("testItem")){
+			Assert.assertTrue(true);}
+		
+		if((double) testItem.getColumn(2) == 100.0){
+			Assert.assertTrue(true);}
+		
+		if((int) testItem.getColumn(3) == 10){
+			Assert.assertTrue(true);}					
 	}
 }
